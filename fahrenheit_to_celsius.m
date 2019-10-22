16558eb235f2a5dd88661cf8964c5302671ac7de
PENGCHAO <pengchao.wang@postgrad.manchester.ac.uk>
2019-10-22 18:31:59 +0100

    Sorry, I am wrong.

--- fahrenheit_to_celsius.m
+++ fahrenheit_to_celsius.m
@@ -2,3 +2,6 @@ function fahrenheit=fahrenheit_to_celsius(fahrenheit)
 %FAHRENHEIT_TO_CELSIUS   Convert Fahrenheit to Celsius
 celsius=(fahrenheit-32)*(5/9);
 end
+
+fprintf('Absolute zero is %g%sF.\n', celsius_to_fahrenheit(kelvin_to_celsius(0)),deg)
+fprintf('WPC 666')
