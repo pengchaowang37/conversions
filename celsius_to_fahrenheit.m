4e02038fbf2e6b15f7966c8436bc4f3582bdf997
PENGCHAO <pengchao.wang@postgrad.manchester.ac.uk>
2019-10-22 17:31:40 +0100

    Add functions to convert between C and F

---
+++ celsius_to_fahrenheit.m
@@ -0,0 +1,4 @@
+function celsius=celsius_to_fahrenheit(celsius)
+%CELSIUS_TO_FAHRENHEIT  Convert Celsius to Fahrenheit
+fahrenheit=celsius*(9/5)+32;
+end
---
+++ fahrenheit_to_celsius.m
@@ -0,0 +1,4 @@
+function fahrenheit=fahrenheit_to_celsius(fahrenheit)
+%FAHRENHEIT_TO_CELSIUS   Convert Fahrenheit to Celsius
+celsius=(fahrenheit-32)*(5/9);
+end
